b6602dc14aa1b74341d0460cbaf6e12519344d0a
abass-adeyemi <abass.abdulyesir@yahoo.com>
2022-02-21 13:09:19 +0100

    car pooling system

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const displayRoutes = require('express-routemap');
+const mySqlConnection = require('./configuration/mysql');
+const { uuid4 } = require('uuidv4');
+
+// const AppRoutes = require('./routes');
+const port = process.env.PORT;
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log(`i am listening on ${port}`);
+	displayRoutes(app);
+});
+
+mySqlConnection.connect((err) => {
+	if (err) throw err.stack;
+	// connected!
+	console.log('successfully connected: ', mySqlConnection.threadId);
+});
+
+// Error 404
+
+app.use((req, res, next) => {
+	res.status(404).send({
+		status: 'error',
+		message: 'Seems you got lost. so sorry',
+	});
+});
